ef8fcce895c177a35ceada05cc30065fc63f54d5
aressss1 <codewithfaizi@gmail.com>
2024-01-29 10:46:03 +0500

    f-

--- app/(LandingPage)/_components/client-banner.tsx
+++ app/(LandingPage)/_components/client-banner.tsx
@@ -3,72 +3,72 @@ import Image from "next/image";
 const ClientBanner = () => {
     const clientRow1 = [
         {
-            src: "/c-1.png",
+            src: "/clients/c-1.png",
             alt: "c-1"
         },
         {
-            src: "/c-2.png",
+            src: "/clients/c-2.png",
             alt: "c-2"
         },
         {
-            src: "/c-3.png",
+            src: "/clients/c-3.png",
             alt: "c-3"
         },
         {
-            src: "/c-4.png",
+            src: "/clients/c-4.png",
             alt: "c-4"
         },
         {
-            src: "/c-5.png",
+            src: "/clients/c-5.png",
             alt: "c-5"
         },
         {
-            src: "/c-6.png",
+            src: "/clients/c-6.png",
             alt: "c-6"
         },
     ];
 
     const clientRow2 = [
         {
-            src: "/c-7.png",
+            src: "/clients/c-7.png",
             alt: "c-7"
         },
         {
-            src: "/c-8.png",
+            src: "/clients/c-8.png",
             alt: "c-8"
         },
         {
-            src: "/c-9.png",
+            src: "/clients/c-9.png",
             alt: "c-9"
         },
         {
-            src: "/c-10.png",
+            src: "/clients/c-10.png",
             alt: "c-10"
         },
         {
-            src: "/c-11.png",
+            src: "/clients/c-11.png",
             alt: "c-11"
         },
         {
-            src: "/c-12.png",
+            src: "/clients/c-12.png",
             alt: "c-12"
         },
     ];
 
     const clientRow3 = [
         {
-            src: "/c-13.png",
+            src: "/clients/c-13.png",
             alt: "c-13"
         },
         {
-            src: "/c-14.png",
+            src: "/clients/c-14.png",
             alt: "c-14"
         },
 
     ]
 
     return (
-        <div className="flex justify-center items-center w-full flex-col py-14 " >
+        <div className="flex justify-center items-center w-full flex-col py-20 " >
             <div className="flex flex-col gap-4" >
                 <div className="text-center text-zinc-950 text-5xl font-bold leading-10">
                     Millions run on Notion every day
