9e126dcbef48acaa7fe31000a249f4feafc102bc
aressss1 <codewithfaizi@gmail.com>
2024-01-29 10:19:31 +0500

    f-

--- app/(LandingPage)/_components/hero.tsx
+++ app/(LandingPage)/_components/hero.tsx
@@ -15,12 +15,12 @@ const Hero = () => {
     return (
         <div className="flex justify-center items-center  py-10 w-full " >
             <div className="flex flex-col-reverse lg:flex-row gap-5" >
-                <div className="flex flex-col gap-6" >
+                <div className="flex flex-col gap-6 text-center " >
                     <div className=" text-zinc-950 text-5xl font-bold ">
                         Tired of Notion?<br />Say hi to Jotion
                     </div>
                     <div className=" text-black text-opacity-60 text-lg font-medium leading-7">
-                        Jotion is the connected workspace where<br />better, faster work happens
+                        Jotion is the connected workspace where better, faster work happens
                     </div>
                     <div>
                         {isLoading && (
