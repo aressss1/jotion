0ab2f8a0ac017c75fddd24ac5e116da253367b1f
aressss1 <codewithfaizi@gmail.com>
2024-01-29 11:47:37 +0500

    f-

---
+++ app/(LandingPage)/_components/new-tools.tsx
@@ -0,0 +1,63 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Image from "next/image";
+
+const NewToolsSection = () => {
+    const toolsContent = [
+        {
+            imgSrc: "/tools/t-1.png",
+            toolName: "Infinite hierarchy",
+            toolDesc: "No limits on nesting pages inside pages for better organization."
+        },
+        {
+            imgSrc: "/tools/t-2.png",
+            toolName: "Tables & databases",
+            toolDesc: "Keep simple or sophisticated records with views, filters, tags, and more."
+        },
+        {
+            imgSrc: "/tools/t-3.png",
+            toolName: "#Markdown & /slash commands ",
+            toolDesc: "Access all of Notion's tools without touching your mouse to stay in flow."
+        },
+        {
+            imgSrc: "/tools/t-4.png",
+            toolName: "Drag & drop",
+            toolDesc: "Get your ideas on the page, then rearrange. Our editor helps you structure your thoughts and plans."
+        },
+    ]
+
+    return (
+        <div className="flex justify-center items-center  py-10 w-full " >
+            <div className="flex flex-col gap-2 text-center " >
+                <div className=" text-zinc-950 text-5xl font-bold">
+                    New tools, new ways to work
+                </div>
+                <div className=" text-black text-opacity-60 text-lg font-medium leading-7">
+                    We also built in some advanced features for you.
+                </div>
+            </div>
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-6 " >
+                {toolsContent.map((tool) => (
+                    <Card key={tool.toolName} className="text-left"  >
+                        <CardHeader>
+                            <div>
+                                <Image
+                                    src={tool.imgSrc}
+                                    alt={tool.toolName}
+                                />
+                            </div>
+                            <CardTitle className="text-zinc-950 text-lg font-bold" >
+                                {tool.toolName}
+                            </CardTitle>
+                            <CardContent className="text-black text-opacity-60 text-base font-normal leading-normal" >
+                                {tool.toolDesc}
+                            </CardContent>
+                        </CardHeader>
+                    </Card>
+                ))}
+            </div>
+
+        </div>
+    );
+}
+
+export default NewToolsSection;
--- app/(LandingPage)/page.tsx
+++ app/(LandingPage)/page.tsx
@@ -1,11 +1,13 @@
 import ClientBanner from "./_components/client-banner";
 import Hero from "./_components/hero";
+import NewToolsSection from "./_components/new-tools";
 
 const LandingPage = () => {
     return (  
         <div className="h-full mt-20 " >
             <Hero />
             <ClientBanner />
+            <NewToolsSection />
         </div>
     );
 }
